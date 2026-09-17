0357a562d2b9ae0603f3db3d0d0e5a8355cb190f
oluwatooki-GA <oluwatooki.g.adegbite@gmail.com>
2026-09-17 18:41:38 +0100

    Add api package inits and fix router import paths in the collator

--- src/financial_rag_agent/api/api.py
+++ src/financial_rag_agent/api/api.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
-from financial_rag_agent.ingestion.router import router as ingestion_router
-from financial_rag_agent.retrieval.router import router as retrieval_router
+from financial_rag_agent.api.routers.ingestion import router as ingestion_router
+from financial_rag_agent.api.routers.retrieval import router as retrieval_router
 
 api_router = APIRouter(prefix="/api/v1")
 api_router.include_router(ingestion_router)
