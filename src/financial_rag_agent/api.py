2bee94c1c42f2089fcbdea9c6d3ecff827ddf640
oluwatooki-GA <oluwatooki.g.adegbite@gmail.com>
2026-09-17 17:49:10 +0100

    Add api.py router collator and main.py at package root (with /api/v1 versioning)

---
+++ src/financial_rag_agent/api.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+
+from financial_rag_agent.ingestion.router import router as ingestion_router
+from financial_rag_agent.retrieval.router import router as retrieval_router
+
+api_router = APIRouter(prefix="/api/v1")
+api_router.include_router(ingestion_router)
+api_router.include_router(retrieval_router)
